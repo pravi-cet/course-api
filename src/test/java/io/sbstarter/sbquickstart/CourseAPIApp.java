eb8a7f34ac2a7da70de962998bfd07df4274ad4a
pravi-cet <ravi.cet@gmail.com>
2018-09-16 08:35:18 -0400

    Starter spring app

---
+++ src/test/java/io/sbstarter/sbquickstart/CourseAPIApp.java
@@ -0,0 +1,14 @@
+package io.sbstarter.sbquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+
+	public static void main(String[] args) {
+		// Intializes container
+		SpringApplication.run(CourseAPIApp.class, args);
+	}
+
+}
