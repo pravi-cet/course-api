62a0e98c7eedf3132e2f38f54b3324b1c0a618e9
pravi-cet <ravi.cet@gmail.com>
2018-09-18 16:25:04 -0400

    Added RestControllers

---
+++ src/main/java/io/sbstarter/sbquickstart/topic/TopicController.java
@@ -0,0 +1,19 @@
+package io.sbstarter.sbquickstart.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("j2ee", "Java EE", "J2EE Description"),
+				new Topic("python", "Python", "Python Description")
+				);
+	}
+}
